52ce2b736322ba27fdb4a76a367a20dd272c8e57
RichDoherty <doherty622@gmail.com>
2020-01-28 10:34:04 -0500

    Finished app

--- client/components/image_detail.js
+++ client/components/image_detail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import ImageScore from './image_score';
 
 const ImageDetail = (props) => {
   // props.image => this is the image object
@@ -14,6 +15,8 @@ const ImageDetail = (props) => {
         <h4 className="media-heading">
           {props.image.title}
         </h4>
+        <p>{props.image.description}</p>
+        <ImageScore ups={props.image.ups} downs={props.image.downs} />
       </div>
     </li>
   );
--- client/main.js
+++ client/main.js
@@ -1,17 +1,36 @@
 // Any JS in here is automatically run for us
 
 // Import the React Library
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import ImageList from './components/image_list';
 
 // Create a component
-const App = () => {
-  return (
-    <div>
-      <ImageList />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { images: [] };
+  }
+
+  componentWillMount() {
+    // Fantastic place to load data!
+    axios.get('https://api.imgur.com/3/gallery/hot/viral/0', {
+    headers: {
+    Authorization: 'Client-ID a2c2091de9eeb73'
+    }
+  }).then(response => this.setState({ images: response.data.data }));
+  }
+
+  render() {
+    console.log(this.state.images);
+    return (
+      <div>
+        <ImageList images={this.state.images}/>
+      </div>
+    );
+  }
 };
 
 // Render this component to the screen
